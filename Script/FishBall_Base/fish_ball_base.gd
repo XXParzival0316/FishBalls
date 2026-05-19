4fb2b9568423ac2504cfefa3fbacdec0697fae6f
XXParzival <xxparzival0316@icloud.com>
2026-05-20 00:01:12 +0800

    修改-鱼蛋脚本使用状态机模式重构，创建-Test场景，方便后续测试，导入-导入了厨房基础瓦片

--- Script/FishBall_Base/fish_ball_base.gd
+++ Script/FishBall_Base/fish_ball_base.gd
@@ -1,25 +1,55 @@
 extends CharacterBody2D
+# Author XXParzival
 
+# 状态机
+enum STATE{
+	FLOOR,
+	FALL,
+}
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED := 300.0
+const JUMP_VELOCITY := -400.0
+const GRAVITY := 1000.0
 
+var active_state := STATE.FLOOR
+
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var direction := Input.get_axis("Left","Right")
+	
+	# 匹配状态
+	match active_state:
+		STATE.FLOOR:
+			# 动画播放
+			if direction:
+				if direction == 1.0:
+					animated_sprite_2d.flip_h = false
+					animated_sprite_2d.play("Walk")
+				else:
+					animated_sprite_2d.flip_h = true
+					animated_sprite_2d.play("Walk")
+			else:
+				animated_sprite_2d.play("Idle")
+			velocity.x = direction * SPEED
+			# 处理跳跃
+			if Input.is_action_just_pressed("Up"):
+				velocity.y = JUMP_VELOCITY
+			if not is_on_floor():
+				active_state = STATE.FALL
+		STATE.FALL:
+			velocity.x = direction * SPEED
+			velocity.y += GRAVITY * delta
+			# 动画播放
+			if direction:
+				if direction == 1.0:
+					animated_sprite_2d.flip_h = false
+					animated_sprite_2d.play("Walk")
+				else:
+					animated_sprite_2d.flip_h = true
+					animated_sprite_2d.play("Walk")
+			if  is_on_floor():
+				velocity.y = 0
+				active_state = STATE.FLOOR
+	
 	move_and_slide()

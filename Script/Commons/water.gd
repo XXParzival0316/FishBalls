e7cd4fe4cb3107384c2151c5b32dab794c0e06cc
Baishu <baishu@qq.com>
2026-05-23 00:04:28 +0800

    Update water.gd
    加了一点点代码

--- Script/Commons/water.gd
+++ Script/Commons/water.gd
@@ -1,11 +1,18 @@
 extends Area2D
 # Author Baishu
 
-# Called when the node enters the scene tree for the first time.
+@export var buoyancy : float = 0.5 #浮力
+@export var slow : float = 0.5 #减速系数
+@export var gravity_in_water : float = 0.5 #在水中的重力
+
 func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+	body_entered.connect(_fall_into_water)
+	body_exited.connect(_exited_the_water)
+	
+func _fall_into_water(player:Node2D):
+	if player.has_method("fall_in_water"): #检测进入的物体身上有没有“fall_in_water"这个函数
+		#target_in_area.append(body)
+		
+		print("fall in water")
+func _exited_the_water(body:Node2D):
 	pass

473c786bfb25c069d80905109cea43682484bba0
Baishu <baishu@qq.com>
2026-06-02 18:46:22 +0800

    敌人追击巡逻状态大体完成

--- Script/Enemy/State/octopus/octopus_chase_state.gd
+++ Script/Enemy/State/octopus/octopus_chase_state.gd
@@ -2,9 +2,39 @@
 #切换回PatrolState：get_parent().change_state(load("res://Script/Enemy/State/octopus/octopus_patrol_state.gd")  )
 extends Node
 
+var octopus : Enemy
+
 func start():
-	get_parent().modulate = Color(0.0, 0.584, 0.778, 1.0)
+	octopus = get_parent()
+	print("播放警戒动画")
+	octopus.is_move = false
+	octopus.animated_sprite_2d.play("warning")
+	await octopus.animated_sprite_2d.animation_finished
+	octopus.is_move = true
+	octopus.animated_sprite_2d.play("chase")
+	octopus.now_speed = octopus.chase_speed
+	octopus.waittime = 0
+	
 
 func _physics_process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_left"):
-		pass
+	follow_player()
+	if octopus.current_enemy == null:
+		await get_tree().create_timer(0.3).timeout
+		octopus.change_state(load("res://Script/Enemy/State/octopus/octopus_patrol_state.gd"))
+
+
+	
+
+func follow_player():
+	if octopus.current_enemy == null:
+		return
+	if octopus.is_ground_ahead(octopus.dir.x) == true:
+		octopus.flip_direction()
+		octopus.current_enemy = null
+		return
+	if octopus.is_ground_ahead(octopus.dir.x) == false:
+		octopus.dir = (octopus.current_enemy.global_position - octopus.global_position).normalized()
+		if octopus.current_enemy.global_position.x - octopus.global_position.x > 0 and octopus.facing_right == false:
+			octopus.flip_direction()
+		elif octopus.current_enemy.global_position.x - octopus.global_position.x < 0 and octopus.facing_right == true:
+			octopus.flip_direction()
--- Script/Enemy/State/octopus/octopus_patrol_state.gd
+++ Script/Enemy/State/octopus/octopus_patrol_state.gd
@@ -2,11 +2,17 @@
 #切换到chasestate：get_parent().change_state(load("res://Script/Enemy/State/octopus/octopus_chase_state.gd"))
 extends Node
 
+var octopus : Enemy
 
 func start():
-	var octopus : Enemy = get_parent()
+	octopus = get_parent()
 	octopus.now_speed = octopus.normal_speed
 	octopus.animated_sprite_2d.play("walk")
+	octopus.waittime = 2
 
 func _physics_process(delta: float) -> void:
-		pass
+		find_player()
+
+func find_player():
+	if octopus.current_enemy != null:
+		octopus.change_state(load("res://Script/Enemy/State/octopus/octopus_chase_state.gd"))

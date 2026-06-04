f27f2458ea9d19ee36d13fae1728ed5372af8089
Baishu <baishu@qq.com>
2026-06-04 13:41:44 +0800

    修改敌人攻击条件使其更连贯

--- Script/Enemy/State/octopus/octopus.gd
+++ Script/Enemy/State/octopus/octopus.gd
@@ -2,7 +2,7 @@
 extends Enemy
 
 @export var current_enemy : Node2D 
-
+@export var hurting_player : Node2D
 func _enter_tree() -> void:
 	super._enter_tree()
 	normal_speed = 100
@@ -14,6 +14,7 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	super._physics_process(delta)
+	hurt_player()
 
 func _on_warning_area_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
@@ -28,17 +29,22 @@ func _on_wall_checker_body_entered(body: Node2D) -> void:
 
 
 func _on_attack_area_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Player"):
-		last_animation = animated_sprite_2d.animation
-		animated_sprite_2d.play("attack")
-		if body.has_method("take_damage") and body.invincible == false:
-			body.take_damage(10)
-			if body.global_position.x - global_position.x > 0:
-				body.apply_knockback(600)   # 向右击退
-			else:
-				body.apply_knockback(-600)  # 向左击退
-
+	hurting_player = body
 
 func _on_attack_area_body_exited(body: Node2D) -> void:
+	hurting_player = null
 	if body.is_in_group("Player"):
 		animated_sprite_2d.play(last_animation)
+
+func hurt_player():
+	if hurting_player != null:
+		if hurting_player.is_in_group("Player"):
+			last_animation = animated_sprite_2d.animation
+			animated_sprite_2d.play("attack")
+			if hurting_player.has_method("take_damage") and hurting_player.invincible == false:
+				hurting_player.take_damage(10)
+				if hurting_player.global_position.x - global_position.x > 0:
+					hurting_player.apply_knockback(600)   # 向右击退
+				else:
+					hurting_player.apply_knockback(-600)  # 向左击退
+	

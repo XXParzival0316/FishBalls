33ceec63fd21d3cc9a7bb5030cf6a8c29dad0945
XXParzival <xxparzival0316@icloud.com>
2026-06-08 22:09:55 +0800

    优化：鱼蛋技能-冰行技能实现tileset复用，防止重复添加

--- Script/FishBall/fish_ball.gd
+++ Script/FishBall/fish_ball.gd
@@ -232,13 +232,15 @@ func frozen()-> void:
 		var collider = ray_cast_2d.get_collider()
 		if collider is TileMapLayer:
 			tilemapLayer = collider
-			# 对所在的TileMapLayer中的tileset进行设置
-			var tile_set = tilemapLayer.tile_set
-			var scene_source = TileSetScenesCollectionSource.new()
-			# 对tileset添加一个新场景源
-			source_id = tile_set.add_source(scene_source)
-			# 对上面添加的场景源中添加冰砖块
-			ice_block_id = scene_source.create_scene_tile(ice_block)
+			# 对所在的TileMapLayer中的tileset进行设置,之前要是添加过了就复用之前的场景源
+			if not source_id and not ice_block_id:
+				var tile_set = tilemapLayer.tile_set
+				var scene_source = TileSetScenesCollectionSource.new()
+				# 对tileset添加一个新场景源
+				source_id = tile_set.add_source(scene_source)
+				# 对上面添加的场景源中添加冰砖块
+				ice_block_id = scene_source.create_scene_tile(ice_block)
+			
 	else:	
 		if source_id and ice_block_id:
 			var target_vector = tilemapLayer.local_to_map(ray_cast_2d.get_collision_point())

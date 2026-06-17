07c63b95d6a9c56a9fc0c1518b87c5dff9fc2e4d
XXParzival <xxparzival0316@icloud.com>
2026-06-17 22:13:15 +0800

    临时提交

--- Script/FishBall/Skills/IceWalk/icewalk.gd
+++ Script/FishBall/Skills/IceWalk/icewalk.gd
@@ -8,6 +8,8 @@ var ice_block_id:int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	set_collision_mask_value(1,false)
+	print(get_collision_mask())
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

60e2426108a5dd19396b16c9c3cc079e1cc00e9d
XXParzival <xxparzival0316@icloud.com>
2026-07-27 17:27:16 +0800

    优化:鱼车设定边界方式和传送点设定方式优化

--- Script/Refrigerator/FishCar.gd
+++ Script/Refrigerator/FishCar.gd
@@ -3,9 +3,6 @@ extends StaticBody2D
 @export_group("基本属性")
 ## 鱼车移动速度
 @export var move_speed: float = 500
-@export var left_bound_x: float = -50.0
-@export var right_bound_x: float = 300.0
-
 @export_group("受到技能影响参数")
 ## 被芥末酱打到后的晕厥时间
 @export var wasabi_syncope_time:float = 5.0
@@ -15,44 +12,45 @@ extends StaticBody2D
 @export var icewalk_fishcar_speed:float = 200
 @onready var rigth_bar_col: CollisionShape2D = $RightBar/CollisionShape2D
 @onready var left_bar_col: CollisionShape2D = $LeftBar/CollisionShape2D
-
-# 记录鱼车是否有被冰霜行者影响过
-var icewalk_effected:bool = false
-
+@onready var left_bound: Node2D = $LeftBound
+@onready var right_bound: Node2D = $RightBound
+@onready var marker_2d: Marker2D = $Marker2D
 
 # 记录鱼车原始移速
 var origin_speed:float
-var move_dir: float = -1.0  # 1向右，-1向左
-var origin_y: float
+var move_dir: float = 1.0  # 1向右，-1向左
+# 记录鱼车是否有被冰霜行者影响过
+var icewalk_effected:bool = false
+var left_bound_x:float
+var right_bound_x:float
+# 玩家TP位置
+var tp_position:Vector2
 
 func _ready() -> void:
+	left_bound_x = left_bound.global_position.x
+	right_bound_x = right_bound.global_position.x
+	tp_position = marker_2d.global_position
 	rigth_bar_col.disabled = true
 	left_bar_col.disabled = true
 	origin_speed = move_speed
-	origin_y = position.y
-	# 限制初始位置在区间内
-	position.x = clamp(position.x, left_bound_x, right_bound_x)
+
 	# 自动播放鱼车动画
 	$AnimatedSprite2D.play()
 
 func _physics_process(delta: float) -> void:
 	# 水平移动
 	position.x += move_speed * move_dir * delta
-
-	# 边界反弹换向
-	if position.x >= right_bound_x:
-		move_dir = -1.0
-	elif position.x <= left_bound_x:
+	# 碰到左边界向右
+	if position.x < left_bound_x:
 		move_dir = 1.0
-
-	# 锁定Y轴，只水平移动
-	position.y = origin_y
-
-	# 跟随移动方向翻转鱼车（适配AnimatedSprite2D）
+	# 碰到右边界向左
+	if position.x > right_bound_x:
+		move_dir = -1.0
+		
+	# 跟随移动方向翻转鱼车
 	$AnimatedSprite2D.flip_h = move_dir < 0
 
 func take_damage(damage):
-	print("鱼车被击中")
 	modulate = Color(1.0, 0.0, 0.0, 1.0)
 	move_speed = 0
 	$HitDetech.monitoring = false
@@ -69,13 +67,14 @@ func take_damage(damage):
 
 # 传送玩家
 func tp_Player(body:Node2D) -> void:
-	body.position = $Marker2D.global_position
-	
+	body.global_position = tp_position
+
 # 撞到玩家传送
 func _on_hit_detech_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
 		tp_Player(body)
 
+# 受到冰霜行者的影响的效果
 func used_icewalk() -> void:
 	# 受到过影响后不再执行
 	if not icewalk_effected:
@@ -86,22 +85,19 @@ func used_icewalk() -> void:
 		rigth_bar_col.disabled = false
 		left_bar_col.disabled = false
 		await get_tree().create_timer(icewalk_time).timeout
-		print("你好")
 		icewalk_effected = false
 		$HitDetech.monitorable = true
 		rigth_bar_col.disabled = true
 		left_bar_col.disabled = true
 		modulate = Color(1.0, 1.0, 1.0, 1.0)
 		move_speed = origin_speed
-		
-	
-	
+
 # 检测玩家时候使用冰霜行者
 func _on_ice_walk_detech_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player") and body.has_signal("using_icewalk"):
 		if not body.using_icewalk.has_connections():
 			body.using_icewalk.connect(used_icewalk)
-		
+
 func _on_ice_walk_detech_body_exited(body: Node2D) -> void:
 	if body.is_in_group("Player") and body.has_signal("using_icewalk"):
 		if body.using_icewalk.has_connections():

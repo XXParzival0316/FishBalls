29f8aaf736943a427ddd1469ba0b555cd3990fc3
XXParzival <xxparzival0316@icloud.com>
2026-08-04 23:43:36 +0800

    修改：项目打包设置 鱼蛋:血量恢复100

--- Script/GM/GM.gd
+++ Script/GM/GM.gd
@@ -7,6 +7,6 @@ func _ready() -> void:
 	dead_menu.visible = false
 	fish_ball.dead.connect(func(): 
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		dead_menu.visible = fish_ball.is_dead
+		dead_menu.visible = true
 		fish_ball.HP = 100
 		)

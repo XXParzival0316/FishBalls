048982397b0a76e6145b8bebdeaf2e0e58928457
XXParzival <xxparzival0316@icloud.com>
2026-08-04 14:58:03 +0800

    新增：死亡菜单

---
+++ Script/GM/GM.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@onready var fish_ball: Player = $FishBall
+@onready var dead_menu: Control = $CanvasLayer/DeadMenu
+
+func _ready() -> void:
+	dead_menu.visible = false
+	fish_ball.dead.connect(func():
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		dead_menu.visible = fish_ball.is_dead
+		)
---
+++ Script/dead_menu.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@onready var restar: Button = $Restar
+@onready var main_meu: Button = $MainMeu
+
+var kitchen:PackedScene = preload("uid://dwn585jhghlg5")
+
+func _on_restar_pressed() -> void:
+	get_tree().reload_current_scene()
+
+func _on_main_meu_pressed() -> void:
+	get_tree().change_scene_to_packed(kitchen)

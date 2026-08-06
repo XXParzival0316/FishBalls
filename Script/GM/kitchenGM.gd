0207fe6d8be2fa237fb86423e614616fbf2789ba
XXParzival <xxparzival0316@icloud.com>
2026-08-06 11:10:15 +0800

    厨房首次加载动画设置

--- Script/GM/kitchenGM.gd
+++ Script/GM/kitchenGM.gd
@@ -4,18 +4,40 @@ extends Node2D
 
 @onready var fish_ball: Player = %FishBall
 @onready var frame_animation: FrameAnimation = $FrameAnimation
-@onready var camera_2d: Camera2D = $Camera2D
-
+@onready var camera_2d: Camera2D = %Camera2D
+var target_zoom = Vector2(1.0,1.0)
+var begin_zoom:bool = false
+var fished_zoom:bool = false
 
 func _ready() -> void:
 	if first_play_game:
+		fish_ball.input_locked = true
+		camera_2d.zoom = Vector2(2.2,2.2)
+		fish_ball.set_collision_layer_value(1,false)
 		frame_animation.visible = true
 		fish_ball.visible = false
-		fish_ball.set_collision_layer_value(1,false)
-		camera_2d.reset_smoothing()
+
+		await get_tree().create_timer(2.0).timeout
+		
 		frame_animation.play_frames()
-		frame_animation.play_fished.connect(func():
+		frame_animation.play_fished.connect(func():begin_zoom = true)
+		
+			
+
+func _physics_process(delta: float) -> void:
+	if first_play_game:
+		if begin_zoom and not fished_zoom:
+			smonth_zoom()
+			if camera_2d.zoom < Vector2(1.1,1.1):
+				fished_zoom = true
+				
+	if fished_zoom:
 			fish_ball.set_collision_layer_value(1,true)
 			fish_ball.visible = true
 			frame_animation.visible = false
-			)
+			fish_ball.input_locked = false
+
+func smonth_zoom()->void:
+	camera_2d.zoom = lerp(camera_2d.zoom, target_zoom, 1 - exp(-5 * 0.01667))
+
+	
